73da3b811b7003523f540409e1a8aada790c9231
Ayman <a.m_ayman9@outlook.com>
2023-04-26 16:49:18 +0200

    Create project Structure

---
+++ employee-managment-system-backend/src/main/java/com/global/ems/EmployeeManagmentSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.global.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagmentSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagmentSystemBackendApplication.class, args);
+	}
+
+}
