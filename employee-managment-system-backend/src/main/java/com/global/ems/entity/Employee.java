bfe3ee1cbec4aa9f1875a531fb33a4ce71903474
Ayman <a.m_ayman9@outlook.com>
2023-04-26 16:54:00 +0200

    Adding Employee Entity Class

---
+++ employee-managment-system-backend/src/main/java/com/global/ems/entity/Employee.java
@@ -0,0 +1,32 @@
+package com.global.ems.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "employee")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotEmpty
+	private String firstName;
+	@NotEmpty
+	private String LastName;
+	private String email;
+
+}
